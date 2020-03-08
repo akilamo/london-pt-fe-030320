6714a2d4c36a5178100abb57b9ba704073eddbe2
akilamo <58670257+akilamo@users.noreply.github.com>
2020-03-08 14:26:25 +0000

    completed exercise 09 intro to functions

--- 09-js-functions-introduction-exercise/tests/index.test.js
+++ 09-js-functions-introduction-exercise/tests/index.test.js
@@ -57,3 +57,64 @@ let city = "somewhere"
 function myCity(city) {
     alert(`My favorite animal is ${city}`);
 }
+
+global.console = {
+	log: jest.fn(),
+	info: jest.fn(),
+	error: jest.fn(),
+	warn: jest.fn()
+};
+
+
+// Exercise 1
+describe("myFavoriteAnimal", () => {
+	test("myFavoriteAnimal is declared and has value with type 'string'", () => {
+		expect(myFavoriteAnimal).toBeDefined();
+		expect(typeof myFavoriteAnimal).toBe("string");
+	});
+});
+
+// Exercise 2
+describe("myAnimalWithLog", () => {
+	test("myAnimalWithLog is declared and log a string with your favorite animal", () => {
+		myAnimalWithLog();
+
+		expect(global.console.log).toHaveBeenCalledWith(
+			"My favorite animal is " + myFavoriteAnimal
+		);
+	});
+});
+
+// Exercise 3
+describe("myAnimalWithWarn", () => {
+	test("myAnimalWithWarn is declared and warn a string with your favorite animal", () => {
+		myAnimalWithWarn();
+
+		expect(global.console.warn).toHaveBeenCalledWith(
+			"My favorite animal is " + myFavoriteAnimal
+		);
+	});
+});
+
+// Exercise 4
+describe("myAnimalWithError", () => {
+	test("myAnimalWithError is declared and error a string with your favorite animal", () => {
+		myAnimalWithError();
+
+		expect(global.console.error).toHaveBeenCalledWith(
+			"My favorite animal is " + myFavoriteAnimal
+		);
+	});
+});
+
+// Exercise 5 try to run it
+global.alert = jest.fn()
+
+describe("myCity", () => {
+	test("myCity is declared and alert been called", () => {
+		myCity();
+
+		expect(global.alert).toHaveBeenCalled(); //there is no console for alert
+	});
+});
+
