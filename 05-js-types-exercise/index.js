4b60268ef9d3169fb572bae677fb4ef52dfe766d
akilamo <58670257+akilamo@users.noreply.github.com>
2020-03-08 10:58:28 +0000

    completed excercise 05

---
+++ 05-js-types-exercise/index.js
@@ -0,0 +1,14 @@
+const greeting = "Hello";
+let name = "Mimi";
+let phrase = (`${greeting}, ${name}!`);
+
+let price = 1.5;
+let quantity = 2;
+let total = price * quantity;
+let age1 = 20, age2 = 25, age3 = 30;
+let averageAge = (age1 + age2 + age3) / 3;
+
+let loggedIn = true;
+let adminUser = false;
+
+let missingInfo;
