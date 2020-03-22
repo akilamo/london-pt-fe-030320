908d05f8392fb180cc0edb787f1bb65773824fd3
akilamo <58670257+akilamo@users.noreply.github.com>
2020-03-22 15:13:18 +0000

    completed exercise 21 - creating objects

--- 21-creating-objects-exercise/index.js
+++ 21-creating-objects-exercise/index.js
@@ -6,6 +6,11 @@
  *
  * PS: use console.log() to see the result
  */
+const user = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20
+};
 
 /**
  * Exercise 2
@@ -15,6 +20,19 @@
  *
  * PS: use console.log() to see the result
  */
+const allTypes = {
+    string: "a string",
+    number: 2,
+    array: ["an","array"],
+    object: {
+        nestedProp1: 1,
+        nestedProp2: 2,
+    },
+    function: function random () {
+        return Math.random();},
+    boolean: true,
+}
+
 
 /**
  * Exercise 3
@@ -25,3 +43,11 @@
  *
  * PS: use console.log() to see the result
  */
+
+ function createMovieObject (name, rating, ticketPrice) {
+     return movie = {
+         name,
+         rating,
+         ticketPrice
+     } ;
+ }
