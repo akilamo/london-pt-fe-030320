bc9549aa69e0c1b7e141243abe168e6c6c033196
akilamo <58670257+akilamo@users.noreply.github.com>
2020-03-24 16:13:20 +0000

    completed exercise 27 for in

--- 26-for-of-loop-exercise/index.js
+++ 26-for-of-loop-exercise/index.js
@@ -11,12 +11,40 @@
  * empty array.
  */
 
+
+function getIntegersOnly(anArray) {
+
+ let newArray =[];
+
+    for (const item of anArray) {
+        if(Number.isInteger(item)) {
+            newArray.push(item);
+        } 
+    }
+    return newArray;
+}
+
+
 /**
  * Exercise 2
  * create a function {getEvenNumbers}, which takes an array of integers
  * and returns a new array with even numbers only, if there is no even
  * integers it should return an empty array
  */
+
+function getEvenNumbers(anArray2) {
+
+    let newArray2 =[];
+   
+       for (const item of anArray2) {
+           if(item %2 == 0 && item!==0) {
+               newArray2.push(item);
+           } 
+       }
+       return newArray2;
+   }
+
+
 
 /**
  * Exercise 3
@@ -27,12 +55,36 @@
  * should return an empty array
  */
 
+function getEvenNumbersFromMixedArray(anArray3) {
+
+    let newArray3 =[];
+   
+       for (const item of anArray3) {
+           if(Number.isInteger(item) && (item %2 == 0) && item!==0) {
+               newArray3.push(item);
+           } 
+       }
+       return newArray3;
+   }
+
+
 /**
  * Exercise 4
  * create a function {getOddNumbers}, which takes an array of integers
  * and returns a new array with odd numbers only, if there is no odd
  * integers it should return an empty array
  */
+function getOddNumbers(anArray4) {
+
+    let newArray4 =[];
+   
+       for (const item of anArray4) {
+           if(item %2 == 1 && item!==0) {
+               newArray4.push(item);
+           } 
+       }
+       return newArray4;
+   }
 
 /**
  * Exercise 5
@@ -43,3 +95,17 @@
  * If your param is [1,2,3,4,5] the result should be
  * [2,1,4,3,6]
  */
+
+function evenOddTransform(anArray5) {
+
+    let newArray5 =[];
+   
+       for (const item of anArray5) {
+           if(item %2 == 1 && item!==0) {
+            newArray5.push(item+1);
+           } else {
+             newArray5.push(item-1);
+           }
+       }
+       return newArray5;
+   }
--- 27-for-in-loop-exercise/index.js
+++ 27-for-in-loop-exercise/index.js
@@ -3,7 +3,13 @@
  * create a function {logProperties} which will
  * take object as a param and console log each property in a format
  * "Property name: {propertyName}"
- */
+ * */
+ 
+function logProperties(object1) {
+    for (const prop in object1) {
+        console.log(`${prop}`);
+    }
+} 
 
 /**
  * Exercise 2
@@ -11,11 +17,27 @@
  * and returns an array of all values.
  */
 
+function getAllValues(object2) {
+    
+    for (const prop in object2) {
+        return Object.values(object2);
+    }
+}
+
 /**
  * Exercise 3
  * Create a function {getAllValuesAsString} that takes an object
  * and returns all values as one string with space separation.
  */
+
+function getAllValuesAsString(object3){
+    let objectValues = [];
+    for (const prop in object3) {
+    objectValues = Object.values(object3);
+    }
+
+    return objectValues.join(' ');
+}
 
 /**
  * Exercise 4
@@ -23,3 +45,36 @@
  * and log property and values as string in a next format:
  * 'PROPERTY: VALUE'
  */
+function propertyValueString(obj4) {
+        for (const prop in obj4) {
+        console.log((`${prop}: ${(obj4[prop])}`)); 
+        }
+    
+}
+
+
+/*
+ function propertyValueString(object4) {
+    let keyObject = [];
+    let valueObject = [];
+    for (const prop in object4) {
+        keyObject = (Object.keys(object4));
+        keyObject.toUpperCase();
+        valueObject = Object.values(object4);
+        valueObject.toUpperCase();
+        console.log(`${keyObject}: ${valueObject}`);
+     
+    console.log(toString(obj4));
+
+
+
+     let anArray = [];
+    for (const prop in obj4) {
+        anArray.push(`${prop}: ${(obj4[prop])}`); 
+        }
+    console.log((anArray.join(' ')).toUpperCase());
+
+    
+    }
+
+ } */
