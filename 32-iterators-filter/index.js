a98db8c64c2863bd65cac9b1732c590222383f58
akilamo <58670257+akilamo@users.noreply.github.com>
2020-03-29 14:51:13 +0100

    completed exercise 32 iterators filter

--- 32-iterators-filter/index.js
+++ 32-iterators-filter/index.js
@@ -4,7 +4,9 @@
  * create a function {noStrings} that takes an array
  * as a parameter and return an array but without strings
  */
-
+ const noStrings = (array) => array.filter(element => typeof element !== 'string');      
+        
+  
 /**
  * Exercise 2
  *
@@ -13,6 +15,9 @@
  * every other type
  */
 
+const justStrings = (array) => array.filter(element => typeof element === 'string'); 
+
+
 /**
  * Exercise 3
  *
@@ -20,6 +25,7 @@
  * an array as a parameter and return an array of strings
  * that are longer than 5 characters
  */
+const justStringsLongerThan5 = (array) => array.filter(element => element.length > 5);
 
 /**
  * Exercise 4
@@ -28,6 +34,7 @@
  * an array as a parameter and return an array of numbers
  * that are more than 13
  */
+const numbersMoreThan13 = (array) => array.filter(element => element > 13);
 
 /**
  * Exercise 5
@@ -35,6 +42,7 @@
  * create a function {objectsOnly} that takes
  * an array as a parameter and return an array of objects
  */
+const objectsOnly = (array) => array.filter(element => typeof element === 'object');
 
 /**
  * Exercise 6
@@ -43,3 +51,6 @@
  * an array as a parameter and return an array of truthy values.
  * IMPORTANT: You still need to keep number 0.
  */
+
+ const truthyOnly = (array) => array.filter(element => (element === 0 || Boolean(element)));
+
