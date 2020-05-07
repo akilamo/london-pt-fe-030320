549f3fecac4ebf460962d3a19f31f1cb31681acf
akilamo <58670257+akilamo@users.noreply.github.com>
2020-05-07 23:33:44 +0100

    tidied up code

--- 51-js-fetch-json/index.js
+++ 51-js-fetch-json/index.js
@@ -26,45 +26,34 @@ const input = document.querySelector("input");
  * 2. view 3 random facts in ".result" element
  */
 
-
  const fetchData = async (url) => {
-   const response = await fetch(url)
-     .then((response) => response.json())
-
-     return response.all;
+   const response = await fetch(url).then((response) => response.json());
+   return response.all;
  };
-
-// console.log(fetchData('https://cat-fact.herokuapp.com/facts'))
-
 
  const shuffleAndSlice = (array) => {
    for (let i = array.length - 1; i > 0; i--) {
      const j = Math.floor(Math.random() * (i + 1));
      [array[i], array[j]] = [array[j], array[i]];
    }
-
-   return array.slice(0,3);
+   return array.slice(0, 3);
  };
 
-
-
-const displayFact = (cat) => {
-  const catFact = document.createElement("li");
-  catFact.innerHTML = `<p class="fact">${cat.text}</p>
+ const displayFact = (cat) => {
+   const catFact = document.createElement("li");
+   catFact.innerHTML = `<p class="fact">${cat.text}</p>
    <p class="author">${cat.user.name.first} ${cat.user.name.last}</p> `;
+   result.appendChild(catFact);
+ };
 
-  result.appendChild(catFact);
-};
+ 
+ button.addEventListener("click", async () => {
+   result.innerHTML = "";
+   let catData = await fetchData(
+     "https://cat-fact.herokuapp.com/facts"
+   ).then((cats) => shuffleAndSlice(cats));
 
-
-button.addEventListener("click", async () => {
-  result.innerHTML = "";
-  let catData = await fetchData(
-    "https://cat-fact.herokuapp.com/facts"
-  ).then((cats) => shuffleAndSlice(cats));
-  // console.log(catData)
-
-  for (const data in catData) {
-    displayFact(catData[data]);
-  }
-});
+   for (const data in catData) {
+     displayFact(catData[data]);
+   }
+ });
