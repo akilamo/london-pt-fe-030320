afd0ba5ba5bfab29eb5648dae44e96db2e16bf63
akilamo <58670257+akilamo@users.noreply.github.com>
2020-06-03 00:51:05 +0100

    failing 2 tests but posting correctly to db

--- 58-react-forms/src/Components/UserForm/UserForm.js
+++ 58-react-forms/src/Components/UserForm/UserForm.js
@@ -21,13 +21,15 @@ const UserForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    postUser(user);
-  //   setUser({
-  //     firstName: "",
-  //     lastName: "",
-  //     age: 0,
-  //     gender: "",
-  //   });
+    if(user.age !==0) { 
+      postUser(user);
+    }
+      setUser({
+      firstName: "",
+      lastName: "",
+      age: 0,
+      gender: "",
+    });
   };
 
   return (
@@ -80,97 +82,6 @@ const UserForm = () => {
     </div>
   );
 };
-
-
-// const UserForm = () => {
-//   const [user, setUser] = useState({
-//     firstName: "",
-//     lastName: "",
-//     age: 0,
-//     genders: "",
-//   });
-//   const postUser = async (user) => {
-//     await fetch("http://localhost:3001/users", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//       },
-//       body: JSON.stringify(user),
-//     })
-//       .then((response) => {
-//         if (response.ok) {
-//           console.log("OK");
-//           return response;
-//         } else {
-//           console.log("Oops something went wrong");
-//         }
-//       })
-//       .catch((error) => {
-//         console.log("Ooops", error);
-//       });
-//   };
-//   const userAdd = (event) => {
-//     setUser({ ...user, [event.target.name]: event.target.value });
-//     event.target.value = "";
-//   };
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     postUser(user);
-//   };
-//   return (
-//     <div className="state">
-//       <form onSubmit={handleSubmit} className="userForm">
-//         <label htmlFor="">First name:</label>
-//         <input
-//           value={user.firstName}
-//           name="firstName"
-//           className="firstName"
-//           type="text"
-//           placeholder="First Name"
-//           onChange={userAdd}
-//           required
-//         />
-//         <label htmlFor=""> Last name:</label>
-//         <input
-//           value={user.lastName}
-//           name="lastName"
-//           className="lastName"
-//           type="text"
-//           placeholder="Last Name"
-//           onChange={userAdd}
-//           required
-//         />
-//         <label htmlFor=""> What is your age? </label>
-//         <input
-//           value={user.age}
-//           name="age"
-//           className="age"
-//           type="number"
-//           placeholder="Age"
-//           onChange={userAdd}
-//           required
-//         />
-//         <label htmlFor=""> Select your gender:</label>
-//         <select
-//           type="select"
-//           className="gender"
-//           name="genders"
-//           value={user.gender}
-//           onChange={userAdd}
-//           required
-//         >
-//           <option> - - - - - - - </option>
-//           <option value="male">male</option>
-//           <option value="female">female</option>
-//           <option value="Prefer, not to say">Prefer, not to say</option>
-//         </select>
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
 
 
 export default UserForm;
