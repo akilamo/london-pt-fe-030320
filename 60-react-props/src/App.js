b4d11587b97efa935c3b08968391843598352132
akilamo <58670257+akilamo@users.noreply.github.com>
2020-06-06 16:06:33 +0100

    ex 60 completed

--- 60-react-props/src/App.js
+++ 60-react-props/src/App.js
@@ -13,11 +13,15 @@ import UserCard from "./components/UserCard/UserCard";
  */
 
 const App = () => {
+  const [users, setUsers] = useState([]);
 
-fetch('http://localhost:3000/contacts').then((res) => res.json()).then((res) => console.log(res))
+  if (users.length === 0) {
+    fetch("http://localhost:3000/contacts")
+      .then((res) => res.json())
+      .then((res) => setUsers(res));
+  }
 
-
-
+  return users.map((user) => <UserCard user={user} />)
 };
 
 export default App;
--- 60-react-props/src/components/UserCard/UserCard.js
+++ 60-react-props/src/components/UserCard/UserCard.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const UserCard = () => {
-  return <div></div>;
+const UserCard = (props) => {
+  const { user } = props;
+  const { name, email, id } = user;
+  return (
+    <div className="card">
+      <ul key={id}>
+        <li>Name: {name}</li>
+        <li>Email: {email}</li>
+      </ul>
+
+    </div>
+  );
 };
 
 export default UserCard;
